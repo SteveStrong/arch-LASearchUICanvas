1daac0f2a7f4c35ce4bebe929743c1a3944d01a9
SteveStrong <sstrong@apprenticesystems.com>
2020-09-10 22:33:36 -0400

    change name

--- src/app/search-and-render/search-advanced.component.ts
+++ src/app/search-and-render/search-advanced.component.ts
@@ -22,7 +22,7 @@ export class SearchAdvancedComponent implements OnInit {
       includeany: [''],
       includeall: [''],
       exactphrase: [''],
-      excludeall: [''],
+      excludeany: [''],
       findingSentence: [false],
       evidenceSentence: [false],
       legalRuleSentence: [false],
@@ -43,7 +43,7 @@ export class SearchAdvancedComponent implements OnInit {
     const includeall = this.f.includeall.value;
     const includeany = this.f.includeany.value;
     const exactphrase = this.f.exactphrase.value;
-    const excludeall = this.f.excludeall.value;
+    const excludeany = this.f.excludeany.value;
     const findingSentence = this.f.findingSentence.value;
     const evidenceSentence = this.f.evidenceSentence.value;
     const legalRuleSentence = this.f.legalRuleSentence.value;
@@ -74,9 +74,9 @@ export class SearchAdvancedComponent implements OnInit {
         includeall,
         includeany,
         exactphrase,
-        excludeall,
+        excludeany,
       };
-      
+
       Toast.info(`advanced search`);
       EmitterService.broadcastCommand(this, TOPIC_AdvancedQuery, [query]);
     }
